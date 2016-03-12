d8b2c4d9779916290138b5fcf9d5a5a22c855ce6
Allan A <rubel@goosemoon.org>
2016-03-12 12:33:00 -1000

    You can post and read postings.
    The flaskr tutorial, minus login and using dataset instead of sqlite.

---
+++ flaskr.py
@@ -0,0 +1,29 @@
+# flaskr microblogging app
+
+import dataset
+
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+# connecting to a SQLite database
+db = dataset.connect('sqlite:///myflasker.db')
+table = db['postings']
+
+# create the application
+app = Flask(__name__)
+
+# show all the posting
+@app.route("/")
+def show_postings():
+	postings = table['title', 'text']
+	return render_template('show_postings.html', postings=postings)
+
+@app.route("/add", methods=['POST'])
+def add_posting():
+	table.insert(dict(title=request.form['title'], text=request.form['text']))
+	flash("New posting successful")
+	return redirect(url_for('show_postings'))
+
+if __name__ == '__main__':
+	app.debug = "TRUE"
+	app.secret_key = "secret"
+	app.run()
