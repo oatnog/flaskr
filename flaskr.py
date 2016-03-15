cb69bac9d18bcdeb55e63928cd235a4d44239569
Allan A <rubel@goosemoon.org>
2016-03-15 09:50:19 -1000

    Simplify. No error handling needed for table.all()
    I like the table['title', 'text'] syntax, but it always returns an
    OrderedDict sorted alphabetically by the first index (title). Worse, it
    barfs if the table is empty. If I just go back to .all() or .find(),
    it's simpler, no errors, and in ID (creation) order.
    Probably these complications are a good argument for using a proper
    schema instead of letting dataset hand everything.  But it works out
    okay for educational purposes.

--- flaskr.py
+++ flaskr.py
@@ -12,21 +12,21 @@ table = db['postings']
 app = Flask(__name__)
 
 # show all the posting
+
+
 @app.route("/")
 def show_postings():
-    try:
-    	postings = table['title', 'text']
-    except:
-        postings = []
+    postings = table.all()
     return render_template('show_postings.html', postings=postings)
 
-@app.route("/add", methods=['POST'])
+
+@app.route("/add", methods=['POST']) # only accept connections which POST
 def add_posting():
-	table.insert(dict(title=request.form['title'], text=request.form['text']))
-	flash("New posting successful")
-	return redirect(url_for('show_postings'))
+    table.insert(dict(title=request.form['title'], text=request.form['text']))
+    flash("New posting successful")
+    return redirect(url_for('show_postings'))
 
 if __name__ == '__main__':
-	app.debug = "TRUE"
-	app.secret_key = "secret"
-	app.run()
+    app.debug = "TRUE"
+    app.secret_key = "secret"
+    app.run()
